6f4b8a2c0791ce9b84cc46c204ca3513b2ba6c47
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-05-06 17:16:19 -0300

    feat: update routes API

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//    return $request->user();
-// })->middleware('auth:sanctum');
-
 Route::get('/alarms', [\App\Http\Controllers\Api\AlarmController::class, 'index']);
+Route::get('/alarms/{alarm}', [\App\Http\Controllers\Api\AlarmController::class, 'show']);
+
+Route::get('/alarms-types', [\App\Http\Controllers\Api\AlarmTypeController::class, 'index']);
+Route::get('/alarms-types/{alarmType}', [\App\Http\Controllers\Api\AlarmTypeController::class, 'show']);
