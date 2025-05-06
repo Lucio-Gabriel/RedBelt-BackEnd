9a38580600671930e4273984dcd8d99e0177c399
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-05-06 17:15:49 -0300

    feat: create controller and model of Alarm

--- app/Http/Controllers/Api/AlarmController.php
+++ app/Http/Controllers/Api/AlarmController.php
@@ -3,62 +3,44 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\AlarmResource;
 use App\Models\Alarm;
 use Illuminate\Http\Request;
 
 class AlarmController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        return Alarm::get();
+        return AlarmResource::collection(Alarm::with('alarmType')->get());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
+        $alarm = Alarm::with('alarmType')->findOrFail($id);
+
+        return new AlarmResource($alarm);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //
