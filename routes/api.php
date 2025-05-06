334c8fe261dc7145e288754eef1468938065bae5
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-05-06 09:21:21 -0300

    style: install laravel pint

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/user', function (Request $request) {
+// Route::get('/user', function (Request $request) {
 //    return $request->user();
-//})->middleware('auth:sanctum');
+// })->middleware('auth:sanctum');
 
 Route::get('/alarms', [\App\Http\Controllers\Api\AlarmController::class, 'index']);
-
