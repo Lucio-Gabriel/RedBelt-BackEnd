d6fea7bf21af75f159ee911d21be606a1e065829
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-05-12 08:37:35 -0300

    feat: updated route API

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/alarms', [\App\Http\Controllers\Api\AlarmController::class, 'index']);
-Route::get('/alarms/{alarm}', [\App\Http\Controllers\Api\AlarmController::class, 'show']);
-Route::post('/alarms', [\App\Http\Controllers\Api\AlarmController::class, 'store']);
-Route::put('/alarms/{alarm}', [\App\Http\Controllers\Api\AlarmController::class, 'update']);
-Route::delete('/alarms/{alarm}', [\App\Http\Controllers\Api\AlarmController::class, 'destroy']);
-
 Route::get('/alarms-types', [\App\Http\Controllers\Api\AlarmTypeController::class, 'index']);
 Route::get('/alarms-types/{alarmType}', [\App\Http\Controllers\Api\AlarmTypeController::class, 'show']);
 Route::post('/alarms-types', [\App\Http\Controllers\Api\AlarmTypeController::class, 'store']);
 Route::put('/alarms-types/{alarmType}', [\App\Http\Controllers\Api\AlarmTypeController::class, 'update']);
 Route::delete('/alarms-types/{alarmType}', [\App\Http\Controllers\Api\AlarmTypeController::class, 'destroy']);
+
+Route::get('/alarms', [\App\Http\Controllers\Api\AlarmController::class, 'index']);
+Route::get('/alarms/{alarm}', [\App\Http\Controllers\Api\AlarmController::class, 'show']);
+Route::post('/alarms', [\App\Http\Controllers\Api\AlarmController::class, 'store']);
+Route::put('/alarms/{alarm}', [\App\Http\Controllers\Api\AlarmController::class, 'update']);
+Route::delete('/alarms/{alarm}', [\App\Http\Controllers\Api\AlarmController::class, 'destroy']);
